fe8050700d2678ddc6378be3b7be29f3645a8b3e
Hernan Rossi <hfdrossi@gmail.com>
2020-06-05 18:56:09 -0700

    Add customeDragLater

--- src/components/CustomDragLayer.tsx
+++ src/components/CustomDragLayer.tsx
@@ -3,11 +3,11 @@ import { CustomDragLayerContainer } from '../styles/styles'
 import { Column } from './Column'
 import React from 'react'
 
-const CustomDragLayer: React.FC = () => {
-  const { isDragging, item } = useDragLayer(monitor => {
+const CustomDragLayer: React.FC = (props) => {
+  const { isDragging, item } = useDragLayer((monitor) => ({
     item: monitor.getItem(),
     isDragging: monitor.isDragging()
-  })
+  }))
 
   return isDragging ? (
     <CustomDragLayerContainer>
